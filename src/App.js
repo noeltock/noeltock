4c9bd8234150e720828a863792fad0de09a0ce3a
Noel Tock <hello@noeltock.com>
2019-01-12 23:13:39 +0100

    Add GA 📊

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
-import Header from './components/Header';
-import FlightsMap from './components/FlightsMap';
-import Credits from './components/Credits';
-import './index.scss';
-import ntDuotone from './img/nt-duotone.jpg';
+import React, { Component } from 'react'
+import Header from './components/Header'
+import FlightsMap from './components/FlightsMap'
+import Credits from './components/Credits'
+import './index.scss'
+import ntDuotone from './img/nt-duotone.jpg'
+import ReactGA from 'react-ga'
+
+ReactGA.initialize('UA-40085355-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 // <img src={ntDuotone} alt="Logo" />
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <div className="split">
-          <div className="col">
-            <div className="content">
-              
+      <div className='App'>
+        <div className='split'>
+          <div className='col'>
+            <div className='content'>
+
               <h1>Noel Tock</h1>
               <h2>About</h2>
               <p>I built my first website in 1995 and have watched the web grow since. I've enjoyed building products, digital nomadism, leading remote teams whilst remaining curious about what's next.</p>
@@ -23,20 +27,20 @@ class App extends Component {
                 Building digital experiences since 1995. Grew from product
                 design and developing SaaS/web applications — to building
                 effective teams and bringing open-source to enterprise/big
-                media. <a href="#">Everything that touches</a> on CMS, DXP, headless, machine
+                media. <a href='#'>Everything that touches</a> on CMS, DXP, headless, machine
                 learning and intelligent/personalised experiences are the kind
               of projects we're working on. If that’s part of your digital
                 strategy, I’d love to hear from you.
               </p>
             </div>
           </div>
-          <div className="col">
+          <div className='col'>
             <FlightsMap />
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
