6561a51c87a106e1248a5103d5335fe9b05808e0
Noel Tock <hello@noeltock.com>
2019-01-12 23:06:36 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Header from './components/Header';
 import FlightsMap from './components/FlightsMap';
 import Credits from './components/Credits';
 import './index.scss';
+import ntDuotone from './img/nt-duotone.jpg';
 
+// <img src={ntDuotone} alt="Logo" />
 class App extends Component {
   render() {
     return (
@@ -11,14 +13,19 @@ class App extends Component {
         <div className="split">
           <div className="col">
             <div className="content">
-              <h2>About me</h2>
+              
+              <h1>Noel Tock</h1>
+              <h2>About</h2>
+              <p>I built my first website in 1995 and have watched the web grow since. I've enjoyed building products, digital nomadism, leading remote teams whilst remaining curious about what's next.</p>
+              <h2>Current</h2>
+              <h2>Past</h2>
               <p>
                 Building digital experiences since 1995. Grew from product
                 design and developing SaaS/web applications — to building
                 effective teams and bringing open-source to enterprise/big
-                media. Everything that touches on CMS, DXP, headless, machine
+                media. <a href="#">Everything that touches</a> on CMS, DXP, headless, machine
                 learning and intelligent/personalised experiences are the kind
-                of projects we're working on. If that’s part of your digital
+              of projects we're working on. If that’s part of your digital
                 strategy, I’d love to hear from you.
               </p>
             </div>
