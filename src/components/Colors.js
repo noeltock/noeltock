522424de8f2da5731652e0cece2e8e2ec8fe2154
Noel Tock <hello@noeltock.com>
2019-01-14 21:34:25 +0100

    proper chart

---
+++ src/components/Colors.js
@@ -0,0 +1,4 @@
+export const colors = {
+  pri: "#ff8065",
+  sec: "#0d3b6d"
+};
--- src/components/D3WPChart.js
+++ src/components/D3WPChart.js
@@ -7,87 +7,142 @@ import {
   VerticalGridLines,
   HorizontalGridLines
 } from "react-vis";
+import { colors } from "./Colors";
 
-const data = [
+const dataShare = [
   {
     x: 2004,
-    interest: 1,
-    y: 1.0
+    y: 1.0 / 2
   },
   {
     x: 2005,
-    interest: 10,
     y: 2.0
   },
   {
     x: 2006,
-    interest: 21,
     y: 3.4
   },
   {
     x: 2007,
-    interest: 28,
+    y: 28,
     y: 5.0
   },
   {
     x: 2008,
-    interest: 42,
     y: 6.4
   },
   {
     x: 2009,
-    interest: 65,
     y: 8.2
   },
   {
     x: 2010,
-    interest: 80,
     y: 10.5
   },
   {
     x: 2011,
-    interest: 94,
     y: 13.1
   },
   {
     x: 2012,
-    interest: 96,
     y: 15.8
   },
   {
     x: 2013,
-    interest: 93,
     y: 17.4
   },
   {
     x: 2014,
-    interest: 91,
     y: 21.0
   },
   {
     x: 2015,
-    interest: 77,
     y: 23.3
   },
   {
     x: 2016,
-    interest: 70,
     y: 25.6
   },
   {
     x: 2017,
-    interest: 68,
     y: 27.3
   },
   {
     x: 2018,
-    interest: 52,
     y: 29.2
   },
   {
     x: 2019,
-    interest: 42,
     y: 32.7
+  }
+];
+
+const shareMax = 100 / 33;
+
+const dataInterest = [
+  {
+    x: 2004,
+    y: 1 / shareMax
+  },
+  {
+    x: 2005,
+    y: 10 / shareMax
+  },
+  {
+    x: 2006,
+    y: 21 / shareMax
+  },
+  {
+    x: 2007,
+    y: 28 / shareMax
+  },
+  {
+    x: 2008,
+    y: 42 / shareMax
+  },
+  {
+    x: 2009,
+    y: 65 / shareMax
+  },
+  {
+    x: 2010,
+    y: 80 / shareMax
+  },
+  {
+    x: 2011,
+    y: 94 / shareMax
+  },
+  {
+    x: 2012,
+    y: 96 / shareMax
+  },
+  {
+    x: 2013,
+    y: 93 / shareMax
+  },
+  {
+    x: 2014,
+    y: 91 / shareMax
+  },
+  {
+    x: 2015,
+    y: 77 / shareMax
+  },
+  {
+    x: 2016,
+    y: 70 / shareMax
+  },
+  {
+    x: 2017,
+    y: 68 / shareMax
+  },
+  {
+    x: 2018,
+    y: 52 / shareMax
+  },
+  {
+    x: 2019,
+    y: 42 / shareMax
   }
 ];
 
@@ -97,15 +152,34 @@ class d3WPChart extends Component {
       <XYPlot height={300} width={700} xDomain={[2004, 2020]}>
         <VerticalGridLines />
         <HorizontalGridLines />
-        <XAxis />
-        <YAxis />
+        <XAxis title="Year" tickFormat={v => parseInt(v)} />
+        <YAxis
+          title="Market Share"
+          tickFormat={v => v + "%"}
+          orientation="left"
+          style={{ title: { fill: colors.sec }, ticks: { fill: colors.sec } }}
+        />
+        <YAxis
+          title="Consumer Interest"
+          tickFormat={v => parseInt(v * shareMax)}
+          orientation="right"
+          style={{ title: { fill: colors.pri }, ticks: { fill: colors.pri } }}
+          tickValues={[0, 5, 10, 15, 20, 25, 30]}
+        />
         <LineSeries
-          data={data}
+          data={dataShare}
           style={{
-            strokeWidth: "3px"
+            stroke: colors.sec,
+            strokeWidth: 3
           }}
-          lineStyle={{ stroke: "red" }}
-          markStyle={{ stroke: "blue" }}
+          curve={"curveMonotoneX"}
+        />
+        <LineSeries
+          data={dataInterest}
+          style={{
+            stroke: colors.pri,
+            strokeWidth: 3
+          }}
           curve={"curveMonotoneX"}
         />
       </XYPlot>
